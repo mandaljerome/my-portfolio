fefda87d5eb0f81333a17c37a5bf5a77a0d5219f
Jerome Mandal <mandaljerome@yahoo.com>
2023-06-06 21:53:28 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment, useState } from 'react'
+import './App.css'
+import { motion } from "framer-motion";
+
+function App() {
+   const [count, setCount] = useState(0)
+
+   return <motion.div className='App'>
+
+   </motion.div>
+}
+
+export default App
